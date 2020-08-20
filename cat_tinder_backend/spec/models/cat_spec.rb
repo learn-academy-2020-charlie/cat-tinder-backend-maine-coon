2eb1af3bbc6730363f8c926181887b3b77a4f016
ryankoch13 <ryan.koch13@gmail.com>
2020-08-20 12:01:07 -0700

    some testing

--- cat_tinder_backend/spec/models/cat_spec.rb
+++ cat_tinder_backend/spec/models/cat_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate presence" do
+    cat = Cat.create(age:4,enjoys:"Long walks on the beach.")
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it "should validate presence" do
+    cat = Cat.create(name:"Roscoe",enjoys:"Long walks on the beach.")
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it "should validate presence" do
+    cat = Cat.create(name:"Roscoe",age:4)
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should make sure age is a number" do
+    cat = Cat.create(name:"Roscoe",age:"4",enjoys:"Long walks on the beach.")
+    expect(cat.age).to_be a_number
+  end
 end
--- cat_tinder_backend/spec/requests/cats_request_spec.rb
+++ cat_tinder_backend/spec/requests/cats_request_spec.rb
@@ -38,8 +38,20 @@ RSpec.describe "Cats", type: :request do
 
         expect(cat.name).to eq 'Bandit'
     end
-
-
-
-
+    it "doesn't create a cat without a name" do
+        cat_params = {
+            cat: {
+            age: 2,
+            enjoys: 'Walks in the park'
+            }
+        }
+        # Send the request to the  server
+        post '/cats', params: cat_params
+        # expect an error if the cat_params does not have a name
+        expect(response.status).to eq 422
+        # Convert the JSON response into a Ruby Hash
+        json = JSON.parse(response.body)
+        # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+        expect(json['name']).to include "can't be blank"
+    end
 end
